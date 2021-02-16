9dcc40530beb9d758d973099e407373c1ff7a8ff
Salah Eddine ZAKA <zakasalaheddine@gmail.com>
2021-02-16 22:33:42 +0100

    update html function calls

--- src/student-loan/index.js
+++ src/student-loan/index.js
@@ -17,7 +17,7 @@ function insertModalIntoPage() {
       height="329pt"
       viewBox="0 0 329.26933 329"
       width="329pt"
-      onclick="hideModal()"
+      onclick="studentLoan.hideModal()"
     >
       <path
         d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0"
@@ -41,7 +41,7 @@ function insertModalIntoPage() {
           />
           <ul class="autocomplete-result-list suggestions__list"></ul>
         </div>
-        <button class="loan__button" onclick="handleModalSubmit()">FIND LOANS</button>
+        <button class="loan__button" onclick="studentLoan.handleModalSubmit()">FIND LOANS</button>
       </div>
     </div>
   </div>`
@@ -121,7 +121,7 @@ function createSearchInput() {
         />
         <ul class="autocomplete-result-list suggestions__list"></ul>
       </div>
-      <button class="search__button" onclick="handleSearchSubmit()">FIND LOANS</button>
+      <button class="search__button" onclick="studentLoan.handleSearchSubmit()">FIND LOANS</button>
     </div>
   </div>
   `
